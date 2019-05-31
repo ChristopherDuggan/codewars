d9b1a911b6031a8c557f05b028c0713f511abcb9
Christopher Duggan <christopherduggan@gmail.com>
2019-05-30 22:44:48 -0400

    cleaned up growth of population

--- growth-of-a-population.js
+++ growth-of-a-population.js
@@ -1,13 +1,8 @@
 function nbYear(p0, percent, aug, p) {
   let i = 0
-  while(p0 <= p) {
-    console.log(p0)
-    console.log(p)
+  while(p0 < p-1) {
     p0 = p0 * ((percent/100)+1) + aug
     i+= 1
   }
   return i
 }
-
-console.log(nbYear(1500, 5, 100, 5000))
-
