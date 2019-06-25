0cd4b43eedb7c8c5eeae20076d0d8e84ea1f0c7d
Christopher Duggan <christopherduggan@gmail.com>
2019-06-25 16:34:18 -0400

    refactor for one-liners

--- valid-parens.js
+++ valid-parens.js
@@ -6,15 +6,9 @@
 function validParentheses(parens){
   let count = 0
   for ( let i = 0; i < parens.length; i++ ) {
-    if ( parens[i] === '(' ) {
-      count++
-    }
-    if ( parens[i] === ')' ) {
-      count--
-    }
-    if ( count < 0 ) {
-      return false
-    }
+    if ( parens[i] === '(' ) count++;
+    if ( parens[i] === ')' ) count--;
+    if ( count < 0 ) return false;
   }
   return ( count === 0)
 }
