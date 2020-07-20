fd0e99df7542ee716b44a386c1072561fc88fe10
Christopher Duggan <christopherduggan@gmail.com>
2020-07-20 13:12:39 -0400

    v. old stuff, syncing

---
+++ java/Order.java
@@ -0,0 +1,19 @@
+/*
+ * Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+
+
+/*
+ * create an output string whose length is equal to that of the input string
+ * loop over the input string. If a word contains a number, place that number in
+ *  num-1 position in the output string
+ *  return the string
